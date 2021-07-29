4c52f72802dada908ac703b4af168685d03e563a
Mo Lin <molin@wanchain.org>
2021-07-29 20:53:28 +0800

    update

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,23 @@
 const Migrations = artifacts.require("Migrations");
+const CommonProxy = artifacts.require("CommonProxy");
+const Layer2BridgeDelegate = artifacts.require("Layer2BridgeDelegate");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+module.exports = async function (deployer) {
+  // deployer.deploy(Migrations);
+  await deployer.deploy(Layer2BridgeDelegate);
+  let layer2bridge = await Layer2BridgeDelegate.deployed();
+  console.log('layer2bridge', layer2bridge.address);
+
+  const proxyAdmin = '0x2AA0175Eb8b0FB818fFF3c518792Cc1a327a1338';
+
+  const admin = '0x4Cf0A877E906DEaD748A41aE7DA8c220E4247D9e';
+
+  const tokenAddress = ''
+
+  await deployer.deploy(CommonProxy, layer2bridge.address, proxyAdmin, '0x');
+
+  let proxy = await Layer2BridgeDelegate.at((await CommonProxy.deployed()).address);
+  console.log('proxy', proxy.address);
+
+  await proxy.initialize(admin, tokenAddress);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -18,7 +18,7 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -71,6 +71,14 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    ropsten: {
+      provider: () => new HDWalletProvider(process.PK, `https://rinkeby.arbitrum.io/rpc`),
+      network_id: "*",       // Ropsten's id
+      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
